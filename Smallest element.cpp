9db1d903671902c28af983b8a1735ba4174616b2
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-05 22:11:05 -0700

    Add files via upload

---
+++ Smallest element.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+class array{
+    private:
+	int ar[50],i,size;
+	public:
+	int getarray()
+	{
+		cout<<"Enter the size of array"<<endl;
+		cin>>size;
+		cout<<"Enter the elements"<<endl;
+		for(i=0;i<size;i++)
+		{
+			cin>>ar[i];
+		}
+		return 0;
+		}
+		 int smallest()
+		 {
+		 	for(i=1;i<size;i++)
+		 	{
+		 		if(ar[0]>ar[i])
+		 		{
+		 			ar[0]=ar[i];
+				 }
+			 }
+			 cout<<"The smallest element is "<<ar[0]<<endl;
+			 return 0;
+		 }
+		void display()
+		{
+			cout<<"The array elements are"<<endl;
+			for(i=0;i<size;i++)
+			{
+				cout<<ar[i]<<endl;
+			}
+		}
+};
+int main()
+{
+	array obj;
+	obj.getarray();
+	obj.display();
+	obj.smallest();
+	return 0;
+}
