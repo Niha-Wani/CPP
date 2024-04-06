43b393294eb12f770705b0239c4099403e298193
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-05 21:42:45 -0700

    Add files via upload

---
+++ Reverse array.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+class array{
+    private:
+	int ar[50],i,temp,j,size;
+	public:
+	int getarray()
+	{
+		cout<<"Enter the size of array"<<endl;
+		cin>>size;
+		cout<<"Enter the elements"<<endl;
+		for(i=0;i<size;i++)
+		{
+			cin>>ar[i];
+		}
+		return 0;
+		}
+		int reverse()
+		{
+			for(i=0,j=size-1;i<size/2;i++,j--)
+			{
+				temp=ar[i];
+				ar[i]=ar[j];
+				ar[j]=temp;
+			}
+			cout<<"Array reversed successfully"<<endl;
+			return 0;
+		}
+		void display()
+		{
+			cout<<"The array elements are"<<endl;
+			for(i=0;i<size;i++)
+			{
+				cout<<ar[i]<<endl;
+			}
+		}
+};
+int main()
+{
+	array obj;
+	obj.getarray();
+	obj.display();
+	obj.reverse();
+	obj.display();
+	return 0;
+}
