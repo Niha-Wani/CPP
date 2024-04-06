43bf4f7de701a3cc78371ad49c189ccfe62c8f21
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-05 21:38:58 -0700

    Add files via upload

---
+++ Insertarray.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+class array
+{
+	private:
+		int ar[70],size,pos,element,i;
+		public:
+		int getarray()
+		{
+			cout<<"Enter the size of array"<<endl;
+			cin>>size;
+			cout<<"Enter the elements"<<endl;
+			for(i=0;i<size;i++)
+			{
+				cin>>ar[i];
+			}
+			}	
+			int insertarray()
+			{
+				cout<<"Enter the position you want to insert"<<endl;
+				cin>>pos;
+				cout<<"Enter the element"<<endl;
+				cin>>element;
+				for(i=size-1;i>=pos;i--)
+				{
+					ar[i+1]=ar[i];
+			    }
+			    ar[pos]=element;
+				}
+				void display()
+				{
+					cout<<"The new elements are"<<endl;
+					for(i=0;i<size+1;i++)
+					{
+						cout<<ar[i];
+					}
+				}
+};
+int main()
+{
+	array obj;
+	obj.getarray();
+	obj.insertarray();
+	obj.display();
+	return 0;
+}
+
