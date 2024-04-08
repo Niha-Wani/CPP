0dafbbb2974a2b6d56242f8088685a17c8c0650e
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-08 03:52:51 -0700

    Update stackInfixTooPostfix .cpp

--- stackInfixTooPostfix (1).cpp
+++ stackInfixTooPostfix (1).cpp
@@ -12,7 +12,7 @@ int precedence (char ch)
     return 0;
  }
  string convert (string infix)                                                 //  A^B*C-C+D/A/(E+F)     
- {
+{
     int i = 0;
     string postfix = "";
     stack <int>s;
@@ -23,30 +23,30 @@ int precedence (char ch)
             postfix=postfix+infix[i];                                                          // A
             i++;
         }
-          else if(infix[i]=='(')
-          {
+        else if(infix[i]=='(')
+         {
             s.push(infix[i]);                                                              // (
             i++;
-          }
-             else if(infix[i]==')')
-             {
-                while(s.top()!='(')                                                      // e.g., (+*/
-               {
-                 postfix+=s.top();                                                        // +*/
-                 s.pop();                                                                // pop +*/
-                }
-                s.pop();                                                                   // pop left bracket (  itself
-                i++;
-              }
-        else            
+	 }
+        else if(infix[i]==')')
+         {
+            while(s.top()!='(')                                                      // e.g., (+*/
+            {
+               postfix+=s.top();                                                        // +*/
+               s.pop();                                                                // pop +*/
+            }
+               s.pop();                                                                   // pop left bracket (  itself
+               i++;
+         }
+           else            
         {
-            while (!s.empty() && precedence(infix[i])<=precedence(s.top()))                  // * < ^     
-		{
-                    postfix+=s.top();                                         // high precedence operator will be moved to postfix
-                    s.pop();                                                   // and pop that high operator from stack
-                }
-                  s.push(infix[i]);                                                 // low prec operator will be pushed to stack
-                  i++;
+             while (!s.empty() && precedence(infix[i])<=precedence(s.top()))                  // * < ^     
+	    {
+               postfix+=s.top();                                         // high precedence operator will be moved to postfix
+               s.pop();                                                   // and pop that high operator from stack
+             }
+                s.push(infix[i]);                                                 // low prec operator will be pushed to stack
+                i++;
         }
     }
     while(!s.empty())                                                         // last remaining operators in stack
@@ -56,8 +56,7 @@ int precedence (char ch)
         }
          cout<<" Postfix is : "<< postfix;                                        // prints postfix conversion  
          return postfix;
- }
-
+}
 int main()
 {
     string infix = "(a*b+c)"; 
