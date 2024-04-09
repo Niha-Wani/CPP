c7c569698f74b2a4d8a1dda8d45441bf1d686183
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-09 08:08:21 -0700

    Update Binary Search Tree.cpp

--- Binary Search Tree.cpp
+++ Binary Search Tree.cpp
@@ -47,43 +47,55 @@ bool search(node *root,int value)
 		return search(root->rightchild,value);
 	}
 }
-void inorder(node *root){
+void inorder(node *root)
+{
 	if(root==NULL)
 	return;
-	else{
+	else
+	{
 		inorder(root->leftchild);
 		cout<<root->data;
 		inorder(root->rightchild);
 	}
 }
-node *findmin(node *root){
+node *findmin(node *root)
+{
 	if(root==NULL)
 	return NULL;
-	else if(root->leftchild==NULL){
+	else if(root->leftchild==NULL)
+	{
 		return root;
 	}
-	else{
+	else
+	{
 	  return findmin(root->leftchild);
 	}
 }
-node *deletion(node *root,int value){
-	if(root==NULL){
+node *deletion(node *root,int value)
+{
+	if(root==NULL)
+	{
 		cout<<"Node not found"<<endl;
 		return root;
 	}
-	else if(value<root->data){
+	else if(value<root->data)
+	{
 		root->leftchild=deletion(root->leftchild,value);
 	}
-	else if(value>root->data){
+	else if(value>root->data)
+	{
 		root->rightchild=deletion(root->rightchild,value);
 	}
-	else{
-		if(root->leftchild==NULL){
+	else
+	{
+		if(root->leftchild==NULL)
+		{
 			node *temp=root->rightchild;
 			delete root;
 			return temp;
 		}
-		else if(root->rightchild==NULL){
+		else if(root->rightchild==NULL)
+		{
 			node *temp=root->leftchild;
 			delete root;
 			return temp;
@@ -92,15 +104,18 @@ node *deletion(node *root,int value){
 		root->data=minright->data;
 		root->rightchild=deletion(root->rightchild,minright->data);
 	}
-	return root;
-}
-int main(){
+	         return root;
+ }
+int main() 
+{
 	node *root=NULL;
 	int choice,value,flag=1;
-	while(flag==1){
+	while(flag==1)
+	{
 		cout<<endl<<"Enter your choice: 1.INSERT 2.SEARCH 3.DISPLAY 4.DELETION 5.EXIT "<<endl;
 		cin>>choice;
-		switch(choice){
+		switch(choice)
+		{
 			case 1: cout<<"Enter the value to insert"<<endl;
 			cin>>value;
 			root=insert(root,value);
@@ -111,7 +126,8 @@ int main(){
 			{
 				cout<<"Value"<<value<<"found in tree"<<endl;
 			}
-			  else{
+			  else
+			{
 				cout<<"Value"<<value <<"not found "<<endl;
 			}
 			break;
@@ -126,10 +142,8 @@ int main(){
 			case 5: flag=0;
 			break;
 			default: cout<<"Invalid choice"<<endl;
-			break;
-				
+			break;	
 		}
 	}
 	return 0;
 }
-
