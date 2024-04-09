61a465398cb4835dc2691d4dfb489d23cd9de7c1
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-09 08:14:22 -0700

    Update AVL.cpp

--- AVL.cpp
+++ AVL.cpp
@@ -7,37 +7,44 @@ struct node
     node* rc;
     int height;
 };
-int getheight(node *root){
+int getheight(node *root)
+{
 	if(root==NULL)
 	return 0;
-	else{
+	else
+	{
 		return root->height;
 	}
 }
-int getbf(node *root){
+int getbf(node *root)
+{
 	if(root==NULL)
 	return 0;
-	else{
+	else
+	{
 		return getheight(root->lc)-getheight(root->rc);
 	}
 }
-node *create(int value){
+node *create(int value)
+{
     node *temp=new node;
     temp->data=value;
     temp->lc=temp->rc=NULL;
     temp->height=1;
     return temp;
 }
-node *rightrotation(node *y){
+node *rightrotation(node *y)
+{
 	node *x=y->lc;
 	node *t=x->rc;
-    x->rc=y;
-    y->lc=t;
+        x->rc=y;
+        y->lc=t;
     y->height=max(getheight(y->lc),getheight(y->rc))+1;
     x->height=max(getheight(x->lc),getheight(x->rc))+1;
     return x;
 }
-node *leftrotation(node *x){
+node *leftrotation(node *x)
+{
 	node *y=x->rc;
 	node *t=y->lc;
 	y->lc=x;
@@ -46,46 +53,56 @@ node *leftrotation(node *x){
 	y->height=max(getheight(y->lc),getheight(y->rc))+1;
 	return y;
 }
-node *insert(node *root,int value){
+node *insert(node *root,int value)
+{
 	if(root==NULL)
 	return create(value);
-	else if(value<root->data){
+	else if(value<root->data)
+	{
 		root->lc=insert(root->lc,value);
 	}
-	else if(value>root->data){
+	else if(value>root->data)
+	{
 		root->rc=insert(root->rc,value);
 	}
-	else{
+	else
+	{
 		return root;
 	}
 	root->height=1+max(getheight(root->lc),getheight(root->rc));
 	int bf=getbf(root);
-	if(bf>1 && value<root->lc->data){
+	if(bf>1 && value<root->lc->data)
+	{
 	return rightrotation(root);
     }
 	else if(bf<-1 && value>root->rc->data){
 		return leftrotation(root);
 	}
-	else if(bf>1 && value>root->lc->data){
+	else if(bf>1 && value>root->lc->data)
+	{
 		root->lc=leftrotation(root->lc);
 		return rightrotation(root);
 	}
-	else if(bf<-1 && value<root->rc->data){
+	else if(bf<-1 && value<root->rc->data)
+	{
 		root->rc=rightrotation(root->rc);
 		return leftrotation(root);
 	}
 	return root;
 }
-void inorder(node *root){
+void inorder(node *root)
+{
 	if(root==NULL)
 	return;
-	else{
+	else
+	{
 		inorder(root->lc);
 		cout<<" "<<root->data;
 		inorder(root->rc);
 	}
 }
-int main(){
+int main()
+{
 	node *root=NULL;
 	int value;
 	cout<<"Enter the value"<<endl;
