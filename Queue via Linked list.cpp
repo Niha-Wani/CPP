e00e24630e0f43d07a51f0680f3d3f09d40f13df
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-10 08:12:49 -0700

    Update Queue via Linked list.cpp

--- Queue via Linked list.cpp
+++ Queue via Linked list.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
 using namespace std;
-struct nodequeue{
+struct nodequeue
+{
 	int data;
 	nodequeue *link;
 };
-class queue{
+class queue
+{
 	private:
-		nodequeue *front=NULL;
-		nodequeue *rear=NULL;
+	nodequeue *front=NULL;
+	nodequeue *rear=NULL;
 	public:
 	void insert(int val);
 	void deletion();
@@ -16,37 +18,47 @@ class queue{
 	void showfront();
 	void showrear();	
 };
-bool queue::isempty(){
-	if(rear==NULL){
-		return true;
+bool queue::isempty()
+{
+	if(rear==NULL)
+	{
+	return true;
 	}
-	else{
-		return false;
+	else
+	{
+	return false;
 	}
 }
-void queue::insert(int val){
+void queue::insert(int val)
+{
 	nodequeue *ptr=new nodequeue();
 	ptr->data=val;
 	ptr->link=NULL;
-	if(isempty()){
+	if(isempty())
+	{
 	    rear=ptr;
 	    front=ptr;
 	}
-	else{
-		rear->link=ptr;
-		rear=ptr;
+	else
+	{
+	     rear->link=ptr;
+	     rear=ptr;
 	}
 }
-void queue::deletion(){
-	if(isempty()){
-		cout<<"Queue is empty"<<endl;
+void queue::deletion()
+{
+	if(isempty())
+	{
+	     cout<<"Queue is empty"<<endl;
 	}
-	else if(front==rear){
-		cout<<"The deleted element from queue is "<<front->data;
-		delete(front);
-		front=rear=NULL;
+	else if(front==rear)
+	{
+	      cout<<"The deleted element from queue is "<<front->data;
+	      delete(front);
+	      front=rear=NULL;
 	}
-	else{
+	else
+	{
 		nodequeue *ptr=front;
 		front=front->link;
 		cout<<"The deleted element is "<<ptr->data<<endl;
@@ -54,37 +66,47 @@ void queue::deletion(){
 		
 	}
 }
-void queue::showfront(){
-	if(isempty()){
+void queue::showfront()
+{
+	if(isempty())
+	{
 		cout<<"Queue is empty"<<endl;
 	}
-	else{
+	else
+	{
 		cout<<endl<<"The element at the front is "<<front->data;
 	}
 }
-void queue::showrear(){
-	if(isempty()){
+void queue::showrear()
+{
+	if(isempty())
+	{
 		cout<<"Queue is empty"<<endl;
 	}
-	else{
+	else
+	{
 		cout<<endl<<"The element at the rear is "<<rear->data;
 	}
 }
-void queue::display(){
-	if(isempty()){
+void queue::display()
+{
+	if(isempty())
+	{
 		cout<<"Queue is empty"<<endl;
 	}
-	else{
+	else
+	{
 		nodequeue *p=front;
 		cout<<endl<<"The elements of the queue are "<<endl;
-		while(p!=NULL){
+		while(p!=NULL)
+		{
 			cout<<" "<<p->data;
 			p=p->link;
 		}
-		
-		}
 	}
-int main(){
+}
+int main()
+{
      queue obj;
 	int choice,val,flag=1;
 	while(flag==1){
