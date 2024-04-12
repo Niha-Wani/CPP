df00fd77a7274673074027f4242ea2d60765d5bc
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-11 20:55:56 -0700

    Update deletionarray.cpp

--- deletionarray.cpp
+++ deletionarray.cpp
@@ -3,40 +3,40 @@ using namespace std;
 class array
 {
 	private:
-		int ar[70],i,pos,size,count;
-		public:
-			int getarray()
+	    int ar[70],i,pos,size,count;
+	public:
+            int getarray()
+            {
+		cout<<"Enter the size of array"<<endl;
+		cin>>size;
+		cout<<"Enter the elements"<<endl;
+		for(i=0;i<size;i++)
 			{
-				cout<<"Enter the size of array"<<endl;
-				cin>>size;
-				cout<<"Enter the elements"<<endl;
-				for(i=0;i<size;i++)
-				{
-					cin>>ar[i];
-				}
-				cout<<"The array elements are"<<endl;
-				for(i=0;i<size;i++)
-				{
-					cout<<ar[i]<<endl;
-				}
+		         	cin>>ar[i];
+			}
+		cout<<"The array elements are"<<endl;
+		for(i=0;i<size;i++)
+			{
+				cout<<ar[i]<<endl;
+			}
 				return 0;
 			}
-			void deletion()
+	      void deletion()
+		{
+			cout<<"Enter the position you want to delete"<<endl;
+			cin>>pos;
+			for(i=0;i<size;i++)
 			{
-				cout<<"Enter the position you want to delete"<<endl;
-				cin>>pos;
-				for(i=0;i<size;i++)
+			     if(i==pos)
+			      {
+				for(int j=i;j<size-1;j++)
 				{
-				if(i==pos)
-				{
-					for(int j=i;j<size-1;j++)
-					{
-						ar[j]=ar[j+1];
-					}
-						count++;
+					ar[j]=ar[j+1];
 				}
-			}
-				if(count==0) 
+				      count++;
+				}
+			 }
+			      if(count==0) 
 				{
 					cout<<"Element not found"<<endl;
 				}
