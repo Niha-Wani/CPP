469f7b186ba7f00195be1b185fec69513fc1b679
Niha Mukhtar Wani <niha78781@gmail.com>
2024-04-23 07:13:37 -0700

    Update queuetolinkedlist.cpp

--- queuetolinkedlist.cpp
+++ queuetolinkedlist.cpp
@@ -2,12 +2,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 using namespace std;
-struct queue {
+struct queue 
+{
     int data;
     struct queue *next;
 
 };
-void add_at_rear(struct queue **base){
+void add_at_rear(struct queue **base)
+{
     struct queue *n ,*temp;
     n=(struct queue*)malloc(sizeof(struct queue));
     cout<<"Enter Data :";
@@ -17,46 +19,39 @@ void add_at_rear(struct queue **base){
     {
         *base=n;
     }
-    else{
+    else
+    {
         temp=*base;
-        while(temp->next !=NULL){
+        while(temp->next !=NULL)
+	{
             temp=temp->next;
-            
-            
-
-
         }
         temp->next=n;
-        
-
     }
     cout<<"Element"<<" "<<n->data<<" "<<"has been added:";
-
-
 }
-void delete_at_front(struct queue **base){
-    
-   
-	  
-	  	struct queue *temp;
-     	
-  	    temp=*base;
-  	    *base=temp->next;
+void delete_at_front(struct queue **base)
+{
+	struct queue *temp;
+  	temp=*base;
+  	*base=temp->next;
         cout<<"Element deleted is "<<temp;
-  	    delete temp; 
-        
+  	delete temp; 
 }
 int main()
 {
     int c;
     struct queue *base=NULL;
-    while (1){
+    while (1)
+    {
         cout<<"<1>to add at rear<2>to delete at front";
         cin>>c;
-        if (c==1){
+        if(c==1)
+	{
             add_at_rear(&base);
         }
-        else if (c==2){
+        else if (c==2)
+	{
             delete_at_front(&base);
         }
     }
